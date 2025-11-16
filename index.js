d63e316bac3aa976fd79a030100470ed29b21c90
Brady <bradytwiz@gmail.com>
2025-11-16 15:34:50 -0700

    initial website setup

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import pg from "pg";
+
+// const db = new pg.Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "blog",
+//   password: "password",
+//   port: 5432,
+// });
+// db.connect();
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get('/analytics', (req, res) => {
+    res.render("analytics.ejs");
+});
+
+app.get('/signup', (req, res) => {
+    res.render("signup.ejs");
+});
+
+app.get('/signin', (req, res) => {
+    res.render("signin.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
